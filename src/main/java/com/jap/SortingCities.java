4a8148a51a5139f37ba4f5fa109dc5203e2e7714
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-22 20:30:02 +0530

    Created a Class for City.

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -2,5 +2,11 @@ package com.jap;
 
 public class SortingCities {
 
+    public static String[] City() {
+        String[] Cities = {"Bern,", "Lucerne,", "InterLaken,", "GrindelWald,", "EngelBerg,", "Geneva,", "Murren,", "Basel,", "Zermatt,", "JungFrauJoch" };
+
+        return new String[0];
+    }
+
 
 }
